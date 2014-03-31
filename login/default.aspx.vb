b42a566317b0ee5a736a4f4ddb55a1038f7a7f76
Charles Knight <cfknight12@gmail.com>
2014-03-31 15:29:25 -0500

    fixed web.config files for role-based security

--- login/default.aspx.vb
+++ login/default.aspx.vb
@@ -2,4 +2,9 @@
 Partial Class login_default
     Inherits System.Web.UI.Page
 
+    'make cursor automatically appear in login box so user doesn't have to click in
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Login1.Focus()
+    End Sub
+
 End Class
